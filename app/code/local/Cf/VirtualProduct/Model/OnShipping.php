fbf615073cdfdc3e63fea9ac50e3d03a60af34be
Victor <bulddozer86@gmail.com>
2015-12-04 00:44:14 +0200

    add sile for checkout

--- app/code/local/Cf/VirtualProduct/Model/OnShipping.php
+++ app/code/local/Cf/VirtualProduct/Model/OnShipping.php
@@ -4,7 +4,8 @@
 class Cf_VirtualProduct_Model_OnShipping extends Mage_Core_Controller_Front_Action
 {
   public function updateShipping(Varien_Event_Observer $observer) {
-    var_dump("Hello");
-    die();
+    Mage::app()->getFrontController()->getResponse()->setRedirect(Mage::getUrl('checkout/onepage/'));
+    Mage::app()->getResponse()->sendResponse();
+    exit;
   }
 }
